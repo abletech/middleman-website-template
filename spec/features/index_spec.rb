faab5f9fe8fdc8152671ca4a11e65d5388b98a82
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2015-03-04 10:45:26 +1300

    Added a spec for the main menu JS interactions.

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -11,4 +11,11 @@ describe "index", js: true, type: :feature, js_errors: true do
       expect(page).to have_content("Site")
     end
   end
+
+  it "has an interactive main menu" do
+    expect(page).to have_selector "#menu_link"
+    find("#menu_link").click
+    expect(page).to have_selector "body:not(.menu_closed)"
+  end
+
 end
