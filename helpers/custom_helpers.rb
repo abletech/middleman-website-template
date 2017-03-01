ef396781714589b692ebdade76a004276c22e047
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-03-01 15:48:37 +1300

    No need to minify inline CSS since Gulp will be doing that.

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -3,16 +3,7 @@ module CustomHelpers
     names.map { |name|
       name += ".css" unless name.include?(".css")
       css_path = sitemap.resources.select { |p| p.source_file.include?(name) }.first
-      output_css = css_path.render
-      rLineBreaks = /\n/
-      rCSSComments = /\/\*[^*]*\*+(?:[^*\/][^*]*\*+)*\//
-      rCommas = /, /
-      rExtraSpaces = / +/
-      rOpenBrackets = / \{ /
-      rColons = /: /
-      rSemiColons = /; /
-      minified_css = output_css.to_s.gsub(rCSSComments,'').gsub(rLineBreaks,'').gsub(rCommas, ',').gsub(rExtraSpaces,' ').gsub(rOpenBrackets, '{').gsub(rColons, ':').gsub(rSemiColons, ';')
-      "<style>#{minified_css}</style>"
+      "<style>#{css_path.render}</style>"
     }.reduce(:+)
   end
 end
