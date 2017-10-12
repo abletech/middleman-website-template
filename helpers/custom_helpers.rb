d485ea391d7711e4634ef86909897b3c64b6da90
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-10-12 15:39:36 +1300

    Tidy up helpers. Add page_class helper.

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,15 +1,7 @@
 module CustomHelpers
 
-  def property_lookup key
-    lookup = {
-      :page_title => 'title',
-      :page_description => 'description',
-      :page_image_url => 'image_url',
-      :page_type => 'type'
-    }
-    return lookup[key]
-  end
-
+  # Inline CSS for the <head> on the homepage
+  # =================================================
   def minify_inline_css css_string
     rLineBreaks = /\n/
     rCSSComments = /\/\*[^*]*\*+(?:[^*\/][^*]*\*+)*\//
@@ -29,6 +21,18 @@ module CustomHelpers
     }.reduce(:+)
   end
 
+  # Property setting on a per-page or site-wide basis
+  # =================================================
+  def property_lookup key
+    lookup = {
+      :page_title => 'title',
+      :page_description => 'description',
+      :page_image_url => 'image_url',
+      :page_type => 'type'
+    }
+    return lookup[key]
+  end
+
   def get_page_value key, default_value = nil
     return default_value if current_page.nil?
 
@@ -36,13 +40,11 @@ module CustomHelpers
     return current_page.data[key] unless current_page.data[key].nil?
 
     # If the key exists in the current_page.data hash using the fallback key
-    lookup_key = property_lookup(key)
-    return current_page.data[lookup_key]
+    return current_page.data[property_lookup(key)]
   end
 
   def get_site_value key
-    lookup_key = property_lookup[key]
-    return data.site[lookup_key]
+    return data.site[property_lookup(key)]
   end
 
   def yield_if_set(key, default_value = nil)
@@ -52,17 +54,26 @@ module CustomHelpers
   end
 
   def fallback_string key
-    # 3-tier fallback:
-    # ================================
+    # Three-tier fallback:
     # 1. if content_for? then yield_content
     # 2. Else try current_page.data
     # 3. Else fallback to data.site
-    yield_if_set(:page_title, get_page_value(:page_title, get_site_value(key)))
+    yield_if_set(key, get_page_value(key, get_site_value(key)))
   end
 
   def get_title
     # e.g. "About us | Acme Inc."
     return "#{fallback_string(:page_title)} | #{data.site.name}"
+  end
+
+  # Class settings
+  # =================================================
+  def get_page_class
+    class_string = "home"
+    if current_page.url != "/"
+      class_string = current_page.url.parameterize
+    end
+    return "page_#{class_string}"
   end
 
 end
