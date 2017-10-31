f8af23284ce36c394fe02ef44bb1c2990b0d44cb
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-10-31 17:08:31 +1300

    Clean up build issues. Getting close to release now.

--- config.rb
+++ config.rb
@@ -67,24 +67,26 @@ configure :build {
 
   ignore '**/.keep'
 
-  activate :favicon_maker do |f|
-    # Requires ImageMagick. `brew install ImageMagick`
-    f.template_dir  = 'source/images'
-    f.icons = {
-      'touch-icon-512x512.png' => [
-        { icon: 'mstile-310x310.png', size: '310x310' },
-        { icon: 'apple-touch-icon-precomposed.png', size: '180x180'}
-      ],
-      'touch-icon-256x256.png' => [
-        { icon: 'apple-touch-icon-152x152-precomposed.png', size: '152x152'},
-        { icon: 'mstile-150x150.png', size: '150x150'}
-      ],
-      'touch-icon-96x96.png' => [
-        { icon: 'mstile-70x70.png', size: '70x70' },
-        { icon: 'apple-touch-icon.png', size: '57x57' },
-        { icon: 'favicon.ico', size: '32x32,16x16' }
-      ]
-    }
+  if data.site.make_icons == true
+    activate :favicon_maker do |f|
+      # Requires ImageMagick. `brew install ImageMagick`
+      f.template_dir  = 'source/img'
+      f.icons = {
+        'touch-icon-512x512.png' => [
+          { icon: 'mstile-310x310.png', size: '310x310' },
+          { icon: 'apple-touch-icon-precomposed.png', size: '180x180'}
+        ],
+        'touch-icon-256x256.png' => [
+          { icon: 'apple-touch-icon-152x152-precomposed.png', size: '152x152'},
+          { icon: 'mstile-150x150.png', size: '150x150'}
+        ],
+        'touch-icon-96x96.png' => [
+          { icon: 'mstile-70x70.png', size: '70x70' },
+          { icon: 'apple-touch-icon.png', size: '57x57' },
+          { icon: 'favicon.ico', size: '32x32,16x16' }
+        ]
+      }
+    end
   end
 
   activate :asset_hash, ignore: %w{
